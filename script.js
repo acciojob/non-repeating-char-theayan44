cf9a260b5915697b2019abc37ca82ffbc1ccc74b
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-09-14 23:22:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr = [];
+	for (let i = 0; i < 26; i++) {
+		arr.push(0);
+	}
+	for (let i = 0; i < str.length; i++) {
+		arr[str.charCodeAt(i) - 97]++;
+	}
+	for(let i=0; i<arr.length; i++){
+		if(arr[i] == 1) return String.fromCharCode(i + 97);
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
